828310a562f22053f463dcb7a0d32179b1e4feab
Leonid Velichko <vlsrnd@gmail.com>
2021-01-12 17:50:44 +0300

    add timerInit function

--- src/js/index.js
+++ src/js/index.js
@@ -8,6 +8,7 @@ import { setSettings } from "./settings/set-settings";
 import { mainSettings } from './settings/main-settings';
 import { Menu } from './interface/menu';
 import { addCellsToGameArea } from './game-area/add-cells-to-game-area';
+import { Timer } from './game-area/header/timer';
 
 const root = document.getElementById('root');
 
@@ -34,6 +35,11 @@ const gameInit = (settings) => {
   listenersInit();
 };
 
+const timerInit = (settings) => {
+  settings.currentGame.timer = new Timer(settings.gameAreaHeaderElements.timer);
+  settings.currentGame.timer.start();
+};
+
 window.addEventListener('load', () => {
   gameInit(mainSettings);
   mainSettings.menu.btn.newGame9x9.onclick = () => gameStart(9, 9, 10, mainSettings);
@@ -48,6 +54,7 @@ const gameStart = (row, column, minesCount, settings) => {
   addCellsToGameArea(settings);
   settings.menu.mainElement.classList.add('hide');
   settings.gameArea.classList.remove('hide');
+  timerInit(settings);
 };
 
 //Only for dev
