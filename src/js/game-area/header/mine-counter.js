e7ba679861d9fe83ff59988357b4d8df9c89fb8a
Leonid Velichko <vlsrnd@gmail.com>
2021-01-12 16:34:31 +0300

    add mine-counter, init create-game-area-header

---
+++ src/js/game-area/header/mine-counter.js
@@ -0,0 +1,17 @@
+export class minesCounter {
+  constructor(settings, element) {
+    this.win = settings.winFunction;
+    this.count = settings.minesCount;
+    this.element = element;
+  }
+  decrease = () => {
+    this.count--;
+    if (this.count <= 0) this.win();
+  }
+  increas = () => {
+    this.count++;
+  }
+  updateElement = () => {
+    this.element.textContent = this.count;
+  }
+}
