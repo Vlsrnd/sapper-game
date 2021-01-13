db1072d66e9805ae11bc04af334a020c59c5ad3f
Leonid Velichko <vlsrnd@gmail.com>
2021-01-13 09:23:02 +0300

    add pause-play.js

--- src/js/game/pause-play.js
+++ src/js/game/pause-play.js
@@ -1,3 +1,11 @@
 export const pausePlay = (settings, button) => {
-  
+  if (settings.currentGame.isPaused) {
+    settings.currentGame.timer.start();
+    button.classList.remove('start-icon');
+    button.classList.add('pause-icon');
+  } else {
+    settings.currentGame.timer.pause();
+    button.classList.remove('pause-icon');
+    button.classList.add('start-icon');
+  }
 };
