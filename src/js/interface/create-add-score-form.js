601158d23b22cb7d7065140124a01a39edbed391
Leonid Velichko <vlsrnd@gmail.com>
2021-01-14 21:58:26 +0300

    add create-add-score-form

---
+++ src/js/interface/create-add-score-form.js
@@ -0,0 +1,12 @@
+export const createScoreForm = (settings) => {
+  const form = document.createElement('form');
+  form.classList.add('win-form', 'invisible');
+  form.innerHTML = '<div>CONGRATULATIONS!!!</div>'
+    + `<div>your result: ${settings.currentGame.timer.time}s</div>`
+    + '<div>input your name:</div>';
+  const input = document.createElement('input');
+  const button = document.createElement('button');
+  button.textContent = 'Done';
+  form.append(input, button);
+  return form;
+};
