4d67e8563b9cf1b2fab61a490005363dc53cc1b1
Leonid Velichko <vlsrnd@gmail.com>
2021-01-15 15:11:35 +0300

    Refractoring listeners-init

--- src/js/init-listeners/listeners-init.js
+++ src/js/init-listeners/listeners-init.js
@@ -2,26 +2,26 @@ import { pausePlay } from '../game/pause-play';
 import { checkIsDoubleButtonClickCreator } from '../init-listeners/check-is-double-button-click-creator';
 import { openMenu } from '../interface/open-menu';
 import { checkAmIWin } from '../game/win/check-am-i-win';
-import { mainSettings } from '../settings/main-settings';
 import { gameStart } from '../game/game-start';
 
 export const listenersInit = (settings) => {
   const checkIsDoubleButtonClick = checkIsDoubleButtonClickCreator(settings);
   settings.gameArea.addEventListener('mousedown', (event) => checkIsDoubleButtonClick(event));
   settings.gameArea.addEventListener('mouseup', (event) => checkIsDoubleButtonClick(event));
-  settings.gameArea.addEventListener('mouseup', (event) => {
-    if (mainSettings.currentGame.isEnd) return;
+
+  const onMouseupCreator = (settings) => (event) => {
+    if (settings.currentGame.isEnd) return;
     if (!event.target.classList.contains('cell-element')) return;
     const cell = settings.cells.get(event.target);
-    if (event.button === 0) {
-      cell.open();
-    } else if (event.button === 1) {
-      cell.open(true);
-    } else if (event.button === 2) {
-      cell.toggleFlag();
-    }
-    checkAmIWin(mainSettings);
-  });
+    if (event.button === 0) cell.open();
+    else if (event.button === 1) cell.open(true);
+    else if (event.button === 2) cell.toggleFlag();
+    checkAmIWin(settings);
+  };
+  const onMouseup = onMouseupCreator(settings);
+
+  settings.gameArea.addEventListener('mouseup', onMouseup);
+
   settings.gameArea.addEventListener('click', (event) => {
     const {pause, menu, restart} = settings.gameAreaHeaderElements; 
     if (event.target === pause) pausePlay(settings, event.target);
