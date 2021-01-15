a3d040c1b1a6781b81f88b80eeee0652a4bce45f
Leonid Velichko <vlsrnd@gmail.com>
2021-01-15 11:41:21 +0300

    fix bug from last commit

--- src/js/game/win/win.js
+++ src/js/game/win/win.js
@@ -2,6 +2,7 @@ import { showAddScoreForm } from '../../interface/show-add-score-form';
 import { winAnimationStart } from './win-animation-start';
 
 export const win = (settings) => () => {
+  if (settings.currentGame.isEnd) return;
   settings.currentGame.timer.pause();
   settings.currentGame.isEnd = true;
   Array.from(settings.cells.values())
