2a873f89898dd5c09c445b82a082fd2c73da5770
Leonid Velichko <vlsrnd@gmail.com>
2021-01-15 12:01:55 +0300

    add disabled for timer and minesCount element. add css animations for menu-icons

--- src/js/game-area/header/create-game-area-header.js
+++ src/js/game-area/header/create-game-area-header.js
@@ -1,3 +1,4 @@
+import { timerInit } from '../../init/timer-init';
 import {createHeaderButton} from './create-header-button';
 
 export const createGameAreaHeader = (settings) => {
@@ -7,8 +8,10 @@ export const createGameAreaHeader = (settings) => {
   const menuElement = createHeaderButton(['menu-icon']);
   const timerElement = createHeaderButton(['timer-icon']);
     timerElement.textContent = '000';
+    timerElement.setAttribute('disabled', true);
   const restartElement = createHeaderButton(['restart-element', 'smile-good']);
   const minesLeftElement = createHeaderButton(['mines-left-icon']);
+    minesLeftElement.setAttribute('disabled', true);
   const pauseElement = createHeaderButton(['pause-icon']);
 
   header.append(menuElement, timerElement, restartElement,
